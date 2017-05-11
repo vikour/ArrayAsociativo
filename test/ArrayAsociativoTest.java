3bd7f038608c6c421d32bcaa64f044e2d2f7ea21
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2017-05-11 14:11:24 +0200

    Test check no key in empty array created.

--- test/ArrayAsociativoTest.java
+++ test/ArrayAsociativoTest.java
@@ -1,4 +1,5 @@
 
+import java.util.NoSuchElementException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,6 +19,14 @@ public class ArrayAsociativoTest {
     public void sizeNotEmpty() {
         ArrayAsociativo a = new ArrayAsociativo("key","value");
         assertNotEquals(0, a.size());
+    }
+    
+    // Buscar el valor correspondiente a una clave en un array vacío
+    @Test(expected = NoSuchElementException.class)
+    public void keyNotFoundEmptyArray() {
+        ArrayAsociativo a= new ArrayAsociativo();
+        String key = "key";
+        a.get("key");
     }
     
 }
